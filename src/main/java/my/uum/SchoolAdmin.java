31c96f609413d623dd9cf952d99cc4fcf2393d59
Ooi Jun Zhen <junzhen2000@gmail.com>
2022-12-28 14:16:31 +0800

    Development RegisterSchoolAdmin-RegisterRoom

--- src/main/java/my/uum/SchoolAdmin.java
+++ src/main/java/my/uum/SchoolAdmin.java
@@ -2,10 +2,12 @@ package my.uum;
 
 public class SchoolAdmin extends Users {
     private String schoolName;
+    private String officeTelNo;
 
-    public SchoolAdmin(String name, String ICNO, String email, String staffID, String telNo, String schoolName) {
+    public SchoolAdmin(String name, String ICNO, String email, String staffID, String telNo, String schoolName, String officeTelNo) {
         super(name, ICNO, email, staffID, telNo);
         this.schoolName = schoolName;
+        this.officeTelNo = officeTelNo;
     }
 
     public void setSchoolName(String schoolName){
@@ -15,5 +17,14 @@ public class SchoolAdmin extends Users {
     public String getSchoolName() {
         return schoolName;
     }
+
+    public void setOfficeTelNo(String officeTelNo){
+        this.schoolName = schoolName;
+    }
+
+    public String getOfficeTelNo() {
+        return officeTelNo;
+    }
+
 }
 
