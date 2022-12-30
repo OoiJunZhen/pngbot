9fcfd87417719004bb6c892e92b888713faf3231
Ooi Jun Zhen <junzhen2000@gmail.com>
2022-12-30 14:20:10 +0800

    update RegisterSchoolAdmin-RegisterRoom

--- src/main/java/my/uum/DatabaseManager.java
+++ src/main/java/my/uum/DatabaseManager.java
@@ -2,6 +2,7 @@ package my.uum;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseManager {
@@ -18,7 +19,6 @@ public class DatabaseManager {
 
     }
 
-
     private Connection connect() {
         // SQLite connection string
         String url = "jdbc:sqlite:database.db";
@@ -29,6 +29,34 @@ public class DatabaseManager {
             System.out.println(e.getMessage());
         }
         return conn;
+    }
+
+
+    public void insertResRoom(String SchoolName, String OfficeTelNo, String BuildingLoc, String RoomName, String RoomDesc, String RoomMaxCap, String RoomType/*, String UserID*/){
+        try{
+            //set dynamic query
+            String AdRegister_room = "INSERT INTO SchoolAd_Register (School_Name, Office_TelNo, Building_Location, Room_Name, Room_Description, Maximum_Capacity, Roomtype/*, UserID*/) VALUES (?,?,?,?,?,?,?/*,?*/)";
+
+
+
+            //Get the preparedStatement Object
+            PreparedStatement preparedStatement = connection.prepareStatement(AdRegister_room);
+
+            //set the values to query
+            preparedStatement.setString(1,SchoolName);
+            preparedStatement.setString(2,OfficeTelNo);
+            preparedStatement.setString(3,BuildingLoc);
+            preparedStatement.setString(4,RoomName);
+            preparedStatement.setString(5,RoomDesc);
+            preparedStatement.setString(6,RoomMaxCap);
+            preparedStatement.setString(7,RoomType);
+            //preparedStatement.setString(8,UserID);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
