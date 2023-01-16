11f780cfa91a9819a3e98ff745d1ac766504de71
asus <atphng@hotmail.com>
2023-01-16 17:33:29 +0800

    On the way

--- src/main/java/my/uum/DatabaseManager.java
+++ src/main/java/my/uum/DatabaseManager.java
@@ -366,7 +366,8 @@ public class DatabaseManager {
             while(rs.next()){
                 if(SchoolHaveRoom(rs.getInt("School_ID"))){
                     list+= "Reply " + rs.getInt("School_ID") +": "+"\n"+
-                            "School Name: " + rs.getString("School_Name") + "\n\n ";
+                            "School Name: " + rs.getString("School_Name") + "\n" +
+                            "Number of rooms: "+ NumberofRooms(rs.getInt("School_ID")) + "\n\n ";
                 }
             }
 
@@ -380,6 +381,27 @@ public class DatabaseManager {
 
 
         return list;
+    }
+
+    public int NumberofRooms(Integer School_ID){
+        int num = 0;
+        String q = "SELECT * FROM Room WHERE School_ID=?";
+
+
+        try(Connection conn = this.connect()){
+            PreparedStatement preparedStatement = conn.prepareStatement(q);
+            preparedStatement.setInt(1,School_ID);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                num += 1;
+            }
+
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+
+        return num;
     }
 
     /**
@@ -2160,10 +2182,11 @@ public class DatabaseManager {
      * @param RoomType
      * @param buildingID
      */
-    public void AddRoom(String RoomName, String RoomDesc, String RoomMaxCap, String RoomType, Integer buildingID){
+    public void AddRoom(String RoomName, String RoomDesc, String RoomMaxCap, String RoomType, Integer SchoolId, Integer buildingID){
         try{
             //set dynamic query
-            String Add_room = "INSERT INTO Room (Room_Name, Room_Description, Maximum_Capacity, Room_Type, /*School_ID,*/ Building_ID) VALUES (?,?,?,?,?)";
+            String Add_room = "INSERT INTO Room (Room_Name, Room_Description, Maximum_Capacity, Room_Type, School_ID, Building_ID) VALUES (?,?,?,?,?,?)";
+
 
             //Get the preparedStatement Object
             PreparedStatement preparedStatement = connection.prepareStatement(Add_room);
@@ -2173,7 +2196,8 @@ public class DatabaseManager {
             preparedStatement.setString(2,RoomDesc);
             preparedStatement.setString(3,RoomMaxCap);
             preparedStatement.setString(4,RoomType);
-            preparedStatement.setInt(5,buildingID);
+            preparedStatement.setInt(5,SchoolId);
+            preparedStatement.setInt(6,buildingID);
 
             preparedStatement.executeUpdate();
 
@@ -2181,7 +2205,6 @@ public class DatabaseManager {
             e.printStackTrace();
         }
     }
-
     /**
      * @author Ooi Jun Zhen
      * Display a list of buildings (Change_info)
@@ -2249,5 +2272,116 @@ public class DatabaseManager {
             System.out.println(e.getMessage());       }
     }
 
+
+
+    /**
+     * @author Ooi Jun Zhen
+     * Get the school id from USER_IC
+     * @param IC
+     * @return school id
+     */
+    public Integer getSchoolId(String IC){
+        Integer School_ID = 0;
+        String x = "SELECT School_ID FROM School_Admin WHERE User_IC=?";
+
+
+        try(Connection conn = this.connect()){
+            PreparedStatement preparedStatement = conn.prepareStatement(x);
+
+            preparedStatement.setString(1,IC);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                return School_ID = rs.getInt("School_ID");
+            }
+
+        }catch (SQLException e){
+            System.out.println(e.getMessage());       }
+        return School_ID;
+    }
+
+    /**
+     * @author Ooi Jun Zhen
+     * Get the delete room list from school id
+     * @param School_ID
+     * @return Delete room list
+     */
+    public String getDeleteRoomList(Integer School_ID){
+        String deleteRoomList = "";
+        String x = "SELECT Room_ID, Room_Name, Room_Type FROM Room WHERE School_ID = ?";
+
+
+        try (Connection conn = this.connect()){
+            PreparedStatement preparedStatement = conn.prepareStatement(x);
+
+            preparedStatement.setInt(1, School_ID);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                deleteRoomList +=
+                        "Reply " + rs.getInt("Room_ID") + ":\n" +
+                                "Room Name: " + rs.getString("Room_Name") + "\n" +
+                                "Room Type: " + rs.getString("Room_Type") + "\n\n";
+            }
+
+
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return deleteRoomList;
+    }
+
+    /**
+     * @author Ooi Jun Zhen
+     * Get delete room info
+     * @param Room_ID
+     * @return Delete room info
+     */
+    public String getDeleteRoomInfo(Integer Room_ID){
+        String deleteRoomInfo = "";
+        String x = "SELECT Room_ID, Room_Name, Room_Description, Maximum_Capacity, Room_Type FROM Room WHERE Room_ID = ?";
+
+
+        try (Connection conn = this.connect()){
+            PreparedStatement preparedStatement = conn.prepareStatement(x);
+
+            preparedStatement.setInt(1, Room_ID);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                deleteRoomInfo =
+                        "Reply " + rs.getInt("Room_ID") + ":\n" +
+                                "Room Name: " + rs.getString("Room_Name") + "\n" +
+                                "Room Description: " + rs.getString("Room_Description") + "\n" +
+                                "Room Maximum Capacity: " + rs.getString("Maximum_Capacity") + "\n" +
+                                "Room Type: " + rs.getString("Room_Type") + "\n\n";
+            }
+
+
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return deleteRoomInfo;
+    }
+
+    /**
+     * @author Ooi Jun Zhen
+     * Delete room from database based on the room id
+     * @param Room_ID
+     * @return Delete room
+     */
+    public String deleteRoom (Integer Room_ID){
+        String deleteRoom = " ";
+        String delRoom = "DELETE From Room WHERE Room_ID = ?";
+
+        try(Connection conn = this.connect()){
+            PreparedStatement preparedStatement = conn.prepareStatement(delRoom);
+
+            preparedStatement.setInt(1, Room_ID);
+            preparedStatement.executeUpdate();
+
+            deleteRoom = "The room is successfully deleted.";
+
+        }catch (SQLException e){
+            System.out.println(e.getMessage());       }
+        return deleteRoom;
+    }
 }
 
